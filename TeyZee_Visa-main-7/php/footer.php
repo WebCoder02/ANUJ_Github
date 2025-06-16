1afda171adae045c3549b046187b81ab45ded715
Web_Coder_Anuj <91132523+WebCoder02@users.noreply.github.com>
2025-06-16 11:58:42 +0530

    Add files via upload
    Uploaded files created by Anuj

---
+++ TeyZee_Visa-main-7/php/footer.php
@@ -0,0 +1,133 @@
+<footer>
+    <div class="container">
+        <div class="footer-grid">
+            <div class="footer-col">
+                <h3>Address</h3>
+                <div class="address">
+                    <p><i class="fas fa-map-marker-alt"></i>
+                        </i> A-302, RG City Centre, DB Gupta Road, Delhi 110011</p>
+                </div>
+                <div class="address">
+                    <p><i class="fas fa-map-marker-alt"></i>
+                        </i> Mumbai - WeWSork Platinum, Marol, Mumbai, Maharashtra, 400059
+                    </p>
+                </div>
+                <a href="https://wa.me/919029027420" class="whatsapp-button"><i class="fab fa-whatsapp"></i> Chat
+                    with us</a>
+            </div>
+
+            <div class="footer-col">
+                <h3>About us</h3>
+                <ul>
+                    <li><a href="mailto:business.tours@kalltrip.com">Email us</a></li>
+                    <li><a href="#">Blogs</a></li>
+                    <li><a href="html\privacy.html">Privacy Policy</a></li>
+                </ul>
+            </div>
+
+            <!-- <div class="footer-col">
+                <h3>Refund policy</h3>
+                <ul>
+                    <li><a href="#">Pricing</a></li>
+                    <li><a href="#">Terms & conditions</a></li>
+                </ul>
+            </div> -->
+            <!-- <div class="footer-links"> -->
+            <!-- <div class="footer-col">
+                <h3>Company</h3>
+                <ul>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Careers</a></li>
+                </ul>
+            </div> -->
+            <div class="footer-col">
+                <h3>Support</h3>
+                <ul>
+                    <li><a href="html\help_center.html">Help Center</a></li>
+                    <li><a href="html\terms_of_service.html">Terms of Service</a></li>
+                    <li><a href="html\refund_policy.html">Refund Policy</a></li>
+                    <li><a href="html\privacy.html">Privacy Policy</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h3>Social</h3>
+                <ul>
+                    <li><a href="https://www.facebook.com/profile.php?id=61575094024472"><i
+                                class="fab fa-facebook"></i> Facebook</a></li>
+                    <li>
+                        <a href="https://x.com/TeyzeeVisas" target="_blank"
+                            style="text-decoration: none; display: inline-flex; align-items: center;">
+                            <span
+                                style="font-weight: bold; font-size: 10px; background-color: rgb(255, 255, 255); color: rgb(160, 151, 151); padding: 0px 4px; border-radius: 8px;">
+                                𝕏
+                            </span>
+                            <span style="margin-left: 6px; color: rgb(255, 255, 255);">X</span>
+                        </a>
+                    </li>
+                    <li><a href="https://www.instagram.com/teyzee_visas/"><i class="fab fa-instagram"></i>
+                            Instagram</a></li>
+                </ul>
+            </div>
+
+        </div>
+
+        <div class="footer-countries">
+            <h3>Read more about visas</h3>
+            <div class="country-links">
+                 <a href="France_Customer.php">France</a> •
+                <a href="Italy_Customer.php">Italy</a> •
+                <a href="Germany_Customer.php">Germany</a> •
+                <a href="Switzerland_Customer.php">Switzerland</a> •
+                <a href="Greece_Customer.php">Greece</a> •
+                <a href="Singapore_Customer.php">Singapore</a> •
+                <a href="Turkey_Customer.php">Turkey</a> •
+                <a href="China_Customer.php">China</a> •
+                <a href="Russia_Customer.php">Russia</a> •
+                <a href="United_Arab_Emirates_Customer.php">United Arab Emirates</a> •
+                <a href="Indonesia_Customer.php">Indonesia</a> •
+                <a href="Vietnam_Customer.php">Vietnam</a> •
+                <a href="United_Kingdom_Customer.php">United Kingdom</a> •
+                <a href="Spain_Customer.php">Spain</a> •
+                <a href="South_Korea_Customer.php">South Korea</a> •
+                <a href="Hungary_Customer.php">Hungary</a> •
+                <a href="Finland_Customer.php">Finland</a> •
+                <a href="Norway_Customer.php">Norway</a> •
+                <a href="Oman_Customer.php">Oman</a> •
+                <a href="Sweden_Customer.php">Sweden</a> •
+                <a href="Austria_Customer.php">Austria</a> •
+                <a href="Denmark_Customer.php">Denmark</a> •
+                <a href="Uzbekistan_Customer.php">Uzbekistan</a> •
+                <a href="Morocco_Customer.php">Morocco</a> •
+                <a href="Netherlands_Customer.php">Netherlands</a> •
+                <a href="Philippines_Customer.php">Philippines</a> •
+                <a href="Saudi_Arabia_Customer.php">Saudi Arabia</a> •
+                <a href="Portugal_Customer.php">Portugal</a> •
+                <a href="Belgium_Customer.php">Belgium</a> •
+                <a href="Lithuania_Customer.php">Lithuania</a> •
+                <a href="Ireland_Customer.php">Ireland</a> •
+                <a href="Luxembourg_Customer.php">Luxembourg</a> •
+                <a href="Hong_Kong_Customer.php">Hong Kong</a> •
+                <a href="Malaysia_Customer.php">Malaysia</a> •
+                <a href="Japan_Customer.php">Japan</a> •
+                <a href="CzechRepublic_Customer.php">Czech Republic</a> •
+                <a href="Romania_Customer.php">Romania</a> •
+                <a href="Slovakia_Customer.php">Slovakia</a> •
+                <a href="Latvia_Customer.php">Latvia</a> •
+                <a href="Estonia_Customer.php">Estonia</a>
+                <a href="#">Azerbaijan</a> •
+                <a href="#">Bahrain</a> •
+                <a href="#">Brazil</a> •
+                <a href="#">Bulgaria</a> •
+                <a href="#">Cambodia</a> •
+                <a href="#">Croatia</a> •
+                <a href="#">Egypt</a> •
+                <a href="#">Georgia</a> •
+                <a href="#">Kenya</a> •
+            </div>
+        </div>
+        <div class="copyright">
+            <p>© 2025 TeyZee Visas. All Rights Reserved.</p>
+        </div>
+    </div>
+</footer>
