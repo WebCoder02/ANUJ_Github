7027379d7671243ead777999dc3e2097f26e0b6f
Sajjad Ali <rizvisajjad80@gmail.com>
2025-08-06 21:52:04 +0530

    Changed customer files for schengen

--- TeyZee_Visa-main-7/php/Georgia_Customer.php
+++ TeyZee_Visa-main-7/php/Georgia_Customer.php
@@ -59,8 +59,17 @@
                             <p>
                                 If you have not registered on TeyzeeVisas, please <strong>Register Now</strong>.<br>
                                 To <strong>Register</strong>, please click the Check Eligibility button:<br>
-                                <a href="https://teyzeevisas.com/php/payment.php?country=Georgia_Customer&amount=499" class="doc-link-special">Check Eligibility</a><br>
-                                Pay ₹499 to check your visa eligibility and get free registration.<br>
+                                <!-- Eligibility Check -->
+<?php 
+$token = base64_encode(json_encode([
+    'country' => 'georgia',
+    'visa_type' => 'eligibility_check', 
+    'amount' => 499,
+    'timestamp' => time()
+]));
+?>
+                                <a href="/payments/payment.php?country=georgia&visa_type=eligibility_check"><button id="check-now" class="doc-link-special">Check Eligibility - Pay ₹499</button></a>
+                                <br>Pay ₹499 to check your visa eligibility and get free registration.<br>
                                 After registration, you will receive your User ID and password on your registered Email ID.<br>
                                 If you have already registered on TeyzeeVisas, please login:<br>
                                 <a href="https://www.teyzeevisas.com/php/login.php?redirect_to=/php/Georgia_Customer.php" class="doc-link-special">Login</a>
@@ -100,8 +109,18 @@
                             <p>
                                 If you have not registered on TeyzeeVisas, please <strong>Register Now</strong>.<br>
                                 To <strong>Register</strong>, please click the Check Eligibility button:<br>
-                                <a href="https://teyzeevisas.com/php/payment.php?country=Georgia_Customer&amount=499" class="doc-link-special">Check Eligibility</a><br>
-                                Pay ₹499 to check your visa eligibility and get free registration.<br>
+                                <!-- Eligibility Check -->
+<?php 
+$token = base64_encode(json_encode([
+    'country' => 'georgia',
+    'visa_type' => 'eligibility_check', 
+    'amount' => 499,
+    'timestamp' => time()
+]));
+?>
+                                <a href="/payments/payment.php?country=georgia&visa_type=eligibility_check"><button id="check-now" class="doc-link-special">Check Eligibility - Pay ₹499</button></a>
+
+                                <br>Pay ₹499 to check your visa eligibility and get free registration.<br>
                                 After registration, you will receive your User ID and password on your registered Email ID.<br>
                                 If you have already registered on TeyzeeVisas, please login:<br>
                                 <a href="https://www.teyzeevisas.com/php/login.php?redirect_to=/php/Georgia_Customer.php" class="doc-link-special">Login</a>
